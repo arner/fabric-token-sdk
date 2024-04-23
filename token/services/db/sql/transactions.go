9ed923a04586212bad74edd6444e2bc95dfe084e
Arne Rutjes <arne123@gmail.com>
2024-04-23 14:03:39 +0200

    add foreign key for transaction endorsement acks
    Signed-off-by: Arne Rutjes <arne123@gmail.com>

--- token/services/db/sql/transactions.go
+++ token/services/db/sql/transactions.go
@@ -281,7 +281,7 @@ func (db *TransactionDB) GetSchema() string {
 		-- tea
 		CREATE TABLE IF NOT EXISTS %s (
 			id CHAR(36) NOT NULL PRIMARY KEY,
-			tx_id TEXT NOT NULL,
+			tx_id TEXT NOT NULL REFERENCES %s,
 			endorser BYTEA NOT NULL,
             sigma BYTEA NOT NULL,
 			stored_at TIMESTAMP NOT NULL
@@ -292,7 +292,7 @@ func (db *TransactionDB) GetSchema() string {
 		db.table.Transactions, db.table.Requests, db.table.Transactions, db.table.Transactions,
 		db.table.Movements, db.table.Requests, db.table.Movements, db.table.Movements,
 		db.table.Validations, db.table.Requests,
-		db.table.TransactionEndorseAck, db.table.TransactionEndorseAck, db.table.TransactionEndorseAck,
+		db.table.TransactionEndorseAck, db.table.Requests, db.table.TransactionEndorseAck, db.table.TransactionEndorseAck,
 	)
 }
 
